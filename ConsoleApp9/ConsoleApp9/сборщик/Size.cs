236f09a188ee4e0cd95ae5012a9c244248399e5f
Lavrusha23rus <lavrusha23rus@mail.ru>
2022-10-08 23:22:02 +0300

    новая ветка 1.1

---
+++ ConsoleApp9/ConsoleApp9/сборщик/Size.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9.сборщик
+{
+    internal class Size
+    {
+        int HeightImageh;
+        int WidthImageh;
+        public int SiZe 
+        {
+            private get
+            {
+
+            }
+        }
+    }
+}
